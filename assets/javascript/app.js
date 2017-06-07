1032cf483466efe4fe473d66fcfee7c299383263
neophoenix733 <neophoenix777@gmail.com>
2017-06-06 19:35:52 -0700

    latest dist, added click event listeners, changed meetup API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
-$("#clear").on("click", function(){
-	
+$(document).ready(function() {
+
+//Clear button for search fields
+$("#clear").on("click", function() {
+  $("input[type=text], textarea").val("");
+  	clearMap();
 });
+
+  $("#submit").on("click", meetUpAjax);
+
+});
+
--- assets/javascript/meetup.js
+++ assets/javascript/meetup.js
@@ -1,29 +1,23 @@
-//google API key
-var apiKeyGoogle = "AIzaSyCE38Ju_63kC2mFECPNzaYLynJ4AuPhlUk";
-//meetup API key
-var apiKeyMeetUp = "7180b838194c635e3776241c137644";
+// Here we construct our URL
+function meetUpAjax (){
+	//google API key
+	var apiKeyGoogle = "AIzaSyCE38Ju_63kC2mFECPNzaYLynJ4AuPhlUk";
+	//meetup API key
+	var apiKeyMeetUp = "7180b838194c635e3776241c137644";
 
-//Clear button for search fields
-$("#clear").on("click", function() {
-  $("input[type=text], textarea").val("");
-});
+	//variables for search button
+	var keyWords = $("#key-words-input").val().trim();
+	var zip = $("#zip-code-input").val().trim();
+	var radius = $("#radius-input").val().trim();
 
-//variables for search button
-var keyWords = $("input").val().trim();
-var zipCode = $("zip").val().trim();
-var radius = $("radius").val().trim();
-
-//global Lat and Long
-var lat;
-var long;
-
-// convert zip code to lat/long coordinates
-function convertZipToCoor(zip, callback) {
 	var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?key=" + apiKey + "&address=" + zip;
+	console.log("Google Maps API:",queryURL);
 
 	var latitude;
 	var longitude;
 
+	// Google Maps AJAX
+	// convert zip code to lat/long coordinates
 	$.ajax({
 		url: queryURL,
 		method: "GET"
@@ -35,34 +29,21 @@ function convertZipToCoor(zip, callback) {
 		console.log("lat:",latitude);
 		console.log("long:",longitude);
 
-		var coordinates = {
-			lat: latitude, 
-			lng: longitude
-		};
-		callback(coordinates);
-	});
-}
+		// var coordinates = {
+		// 	lat: latitude, 
+		// 	lng: longitude
+		// };
 
-
-
-// Here we construct our URL
-function meetUpAjax (){
-	var zip;
-
-	convertZipToCoor(zip,function(coordinates) {
-		// do stuff with the coordinates
-		// attributes are coordinates.lat and coordinates.lng
-		lat = coordinates.lat;
-		long = coordinates.long;
-	});
-
-//AJAX
-var queryURL = "https://api.meetup.com/fine/events?&text=" + keyWords +"&lat="+ lat+"&lon="+ long+"&radius= "+radius;
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-        }).done(function(response) {
-          console.log(queryURL);
-          console.log(response);
+		//Meetup AJAX
+		queryURL = "https://api.meetup.com/fine/events?&text=" + keyWords +"&lat="+ latitude+"&lon="+ longitude+"&radius="+radius;
+	    console.log("Meetup API:",queryURL);
+	    $.ajax({
+	    	url: queryURL,
+	   		method: "GET",
+	   		dataType: "jsonp"
+	    }).done(function(response) {
+	        console.log(queryURL);
+	        console.log(response);
 		});
+	});
 }
